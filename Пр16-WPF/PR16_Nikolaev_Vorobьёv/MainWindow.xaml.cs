639ed7d80456c7c14b39f7c6dc5f655c84dba123
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2024-02-22 18:29:43 +0300

    zip -> vs 2022

---
+++ Пр16-WPF/PR16_Nikolaev_Vorobьёv/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace PR16_Nikolaev_Vorobьёv
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Логин: {textL.Text}\n\rПароль: {textP.Text}");
+
+        }
+    }
+}
